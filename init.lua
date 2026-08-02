e0d2cc6203d53590b50f5fe9570b7a74a601aead
Moustapha <hello@moustapha.dev>
2026-08-02 23:10:16 +0000

    use catppuccin theme

--- init.lua
+++ init.lua
@@ -321,10 +321,14 @@ do
     },
   }
 
+  vim.pack.add { { src = gh 'catppuccin/nvim', name = 'catppuccin' } }
+  require('catppuccin').setup { }
+
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  -- vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'catppuccin'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
